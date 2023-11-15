91025a8acbc885dd76af199d1792b36bad020ad7
madelcamp <mdcgithubprojects@gmail.com>
2023-11-15 10:58:43 -0300

    minor modifications

--- variables_and_data_types/bin/example1.dart
+++ variables_and_data_types/bin/example1.dart
@@ -4,4 +4,6 @@ void main(List<String> args) {
   print(name);
   // Invalid code.
   // name = 'Hello'; # Constant variables cannot be modified later.
+
+  // any modification to a constant is not allowed
 }
--- variables_and_data_types/bin/example2.dart
+++ variables_and_data_types/bin/example2.dart
@@ -1,5 +1,5 @@
 void main(List<String> args) {
-  // finals only prevent reassigning, but allow modifying.
+  // finals only prevent reassigning, but allow modifying internally.
   final age = [1, 2, 3];
   print(age);
 
